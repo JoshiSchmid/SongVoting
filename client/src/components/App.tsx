b813e139c705703393544d4be470d0bc1fd3ec87
JoshiSchmid <jschmid@artiso.com>
2019-11-25 17:32:43 +0100

    Change BackgroundColor to black

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,7 +4,9 @@ import Header from './Header';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div style={{
+      backgroundColor: 'black'
+    }}>
       <Header/>
       <VotingSession />
     </div>
