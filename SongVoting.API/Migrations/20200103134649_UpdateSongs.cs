d3cc78bf701eeab216f7bf50585cff599c8a3aec
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2020-01-03 15:38:29 +0100

    Add UpdateSongs Migration

---
+++ SongVoting.API/Migrations/20200103134649_UpdateSongs.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SongVoting.API.Migrations
+{
+    public partial class UpdateSongs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9059), "7KVAuyWq49C37WWRgfVFJq" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9505), "0gI1wucmc2djSV3e25LqCd" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9522), "5Bp8uC0h4CjxENVBnahOkD" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9523), "1Yqj7oGrttZH3JX2RYTmzS" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9523), "6Nenm0bjHwjCbTet7Zb9VH" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9524), "78twEEzRCHmOzgRn7wAl8I" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9525), "3agx8NSEa7IPUxvbQs5Xax" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9525), "7mj5fT0Biju5jR41m9ghUl" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9526), "5lubagonZYGdyrfbBsOYdy" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9526), "2a1aibn4UgSkeTaxJSWw1L" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9527), "3i65fj9NX9PPNTDKyhlWvP" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9527), "5AcdaSVQfLcUKMaqchfBie" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9528), "5SDcksP8En1l6RtTY1wzHc" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 14,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9529), "0rH0mprtecH3grD9HFM5AD" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9529), "4G29gmjS7Z2nEFeM7FA6wx" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 16,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9530), "50lW1fKoDtyKaiR2bR7ksl" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9530), "0pvGqtwpBePPYdEVzdtXWe" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9531), "0jxIbgW2FoPhqgTh5qhegg" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9531), "5vMzNmHm2ec2zrqiYhkQzZ" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 20,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9532), "6tCueZxmTKZYpQMWXQtCiB" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 21,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 3, 13, 46, 49, 608, DateTimeKind.Utc).AddTicks(9567), "7L51zRDwhfvQR63Ntzkf0E" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(2575), "3KTikFVuiStwKvcIW0Y0uJ" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3031), "5vMzNmHm2ec2zrqiYhkQzZ" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3049), "0rH0mprtecH3grD9HFM5AD" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3050), "4G29gmjS7Z2nEFeM7FA6wx" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3050), "1Yqj7oGrttZH3JX2RYTmzS" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3051), "5Bp8uC0h4CjxENVBnahOkD" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3052), "2a1aibn4UgSkeTaxJSWw1L" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3052), "78twEEzRCHmOzgRn7wAl8I" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3053), "7mj5fT0Biju5jR41m9ghUl" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3053), "5AcdaSVQfLcUKMaqchfBie" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3054), "5lubagonZYGdyrfbBsOYdy" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3054), "5HWBaiVcqnaUf3BI4tfYG5" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3055), "0pvGqtwpBePPYdEVzdtXWe" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 14,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3056), "3i65fj9NX9PPNTDKyhlWvP" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3056), "4EiKZxDgW0sbT8BZ9Gl6dC" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 16,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3057), "6Nenm0bjHwjCbTet7Zb9VH" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3057), "0gI1wucmc2djSV3e25LqCd" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3058), "5SDcksP8En1l6RtTY1wzHc" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3058), "0jxIbgW2FoPhqgTh5qhegg" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 20,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3059), "3agx8NSEa7IPUxvbQs5Xax" });
+
+            migrationBuilder.UpdateData(
+                table: "SpotifyTracks",
+                keyColumn: "Id",
+                keyValue: 21,
+                columns: new[] { "Added", "SpotifyId" },
+                values: new object[] { new DateTime(2020, 1, 2, 17, 26, 45, 869, DateTimeKind.Utc).AddTicks(3060), "50lW1fKoDtyKaiR2bR7ksl" });
+        }
+    }
+}
